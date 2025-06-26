703249b33a917dfc8538b1bc8da464869e6aace8
Makenzie-N <kenzie.nightingale@education.nsw.gov.au>
2025-06-26 20:54:01 +1000

    Create challenge_B.py

---
+++ challenge_B.py
@@ -0,0 +1,12 @@
+username = str(input("What is your Minecraft username?"))
+woodblocks = int(input("How many wood blocks do you have?"))     
+weight = float(input("How heavy is each block? (in kg's)"))
+daytime = bool(input("Is it daytime?"))
+
+while woodblocks <= 10:
+collect = int(input("How many more woodblocks would you like to collect?"))
+  woodblocks = woodblocks + 1
+if woodblocks >= 10
+break
+
+print(f"Your username is {username}. You have collected {woodblocks}. The total weight of your woodblocks is {weight * woodblocks}kg. It is {daytime} therfore it is {}.")
