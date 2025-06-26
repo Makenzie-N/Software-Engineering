4e61377d7e171d6aadfea3fb1233c255fda0c68b
Makenzie-N <kenzie.nightingale@education.nsw.gov.au>
2025-06-26 20:16:50 +1000

    Update pracRocket.py

--- pracRocket.py
+++ pracRocket.py
@@ -1,29 +1,14 @@
-// PseudoCode
-
-start the program
-    ground = 0
-    maxHeight = 100
-    rocketLocation = 0
-    
-    while rocketLocation <= maxHeight
-        output the rocket height
-        increase the rocketLocation by one meter
-    
-    when the rocket has reached maxHeight
-        output maxHeight reached
-    
-    While the rocketLocation > 0
-        output "rocket falling"
-        decrease the rocketLocation by 1
-        output "new loacation"
-    
-    When rocketLocation = 0
-        output "rocket has landed"
-end the program
-
 ground = 0
 maxHeight = 100
 rocketLocation = 0
-rocketHeight = 0
 while rocketLocation <= maxHeight:
-    print(rocketHeight)
+    print(rocketLocation)
+    rocketLocation = rocketLocation + 1
+if rocketHeight >= maxHeight:
+    print("Max height reached.")
+while rocketLocation > 0:
+    print("Rocket falling...")
+    rocketLocation = rocketLocation - 1
+    print(f"New Location is {rocketLocation}")
+if rocketLocation = 0
+    print("Rocket has landed")
