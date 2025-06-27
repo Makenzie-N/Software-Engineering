3f2cb08ea2d68384ed5b418c3d599f2760e15405
Makenzie-N <kenzie.nightingale@education.nsw.gov.au>
2025-06-27 21:31:54 +1000

    Update challenge_B.py

--- challenge_B.py
+++ challenge_B.py
@@ -1,17 +1,19 @@
 username = str(input("What is your Minecraft username?"))
 woodblocks = int(input("How many wood blocks do you currently have?"))     
 weight = float(input("How much does one block weigh (kg)?"))
-daytime = bool(input("Is it daytime?"))
+daytime = bool(input("Is it daytime in the game? (yes/no):"))
 
+print(f"You have {woodblocks}. You need {10 - woodblocks} more.")
 while woodblocks <= 10:
-collect = int(input("How many more woodblocks would you like to collect?"))
-  woodblocks = woodblocks + 1
-if woodblocks >= 10
-break
+ want = int(input("How many more woodblocks would you like to collect?"))
+ woodblocks += want
+ if woodblocks >= 10:
+   print("Collection complete!")
+    break   
 
 print(f"Your username is {username}. You have collected {woodblocks}. The total weight of your woodblocks is {weight * woodblocks}kg.")
 
-if daytime = yes:
-  print("It is daytime, therfore it is safe to build.")
+if daytime:
+  print("It is daytime.")
 else:
-  print("It is nighttime, therefore it is not safe to build.")
+  print("It's night. Watch out for mobs!")
