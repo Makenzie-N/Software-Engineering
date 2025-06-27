bb4b13df901ed1494d07c76c9fb9f12ec5a0df8f
Makenzie-N <kenzie.nightingale@education.nsw.gov.au>
2025-06-27 22:41:20 +1000

    Update challenge_B.py

--- challenge_B.py
+++ challenge_B.py
@@ -4,15 +4,16 @@ weight = float(input("How much does one block weigh (kg)?"))
 daytime = bool(input("Is it daytime in the game? (yes/no):"))
 
 print(f"You have {woodblocks}. You need {10 - woodblocks} more.")
-while woodblocks <= 10:
- want = int(input("How many more woodblocks would you like to collect?"))
+while woodblocks < 10:
+ want = int(input("How many more blocks would you like to collect?"))
  woodblocks += want
  if woodblocks >= 10:
    print("Collection complete!")
-    break   
+ break   
 
-print(f"Your username is {username}. You have collected {woodblocks}. The total weight of your woodblocks is {weight * woodblocks}kg.")
-
+print(f"Player: {username}")
+print(f"Total blocks: {woodblocks}")
+print(f"Total weight: {weight * woodblocks}kg")
 if daytime:
   print("It is daytime.")
 else:
