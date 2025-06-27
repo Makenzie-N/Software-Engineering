2cfb3172fd32d4c7b71ed222637a971ea9ee7426
Makenzie-N <kenzie.nightingale@education.nsw.gov.au>
2025-06-27 22:42:38 +1000

    Update challenge_B.py

--- challenge_B.py
+++ challenge_B.py
@@ -1,11 +1,11 @@
-username = str(input("What is your Minecraft username?"))
-woodblocks = int(input("How many wood blocks do you currently have?"))     
-weight = float(input("How much does one block weigh (kg)?"))
-daytime = bool(input("Is it daytime in the game? (yes/no):"))
+username = str(input("What is your Minecraft username? "))
+woodblocks = int(input("How many wood blocks do you currently have? "))     
+weight = float(input("How much does one block weigh (kg)? "))
+daytime = bool(input("Is it daytime in the game? (yes/no): "))
 
 print(f"You have {woodblocks}. You need {10 - woodblocks} more.")
 while woodblocks < 10:
- want = int(input("How many more blocks would you like to collect?"))
+ want = int(input("How many more blocks would you like to collect? "))
  woodblocks += want
  if woodblocks >= 10:
    print("Collection complete!")
