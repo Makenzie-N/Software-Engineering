07823028c598adf48b932cc4dd335d00dd3d8c61
Makenzie-N <kenzie.nightingale@education.nsw.gov.au>
2025-07-02 19:37:40 +1000

    Create pracs_choiceslists.py

---
+++ pracs_choiceslists.py
@@ -0,0 +1,33 @@
+# prac 5
+mylist = ["apple", "banana", "cherry"]
+print(mylist)
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
+print(thislist[1])
+print(thislist[0])
+# indexing
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+# accessing values between values
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:4])
+# accessing from the beginning to the position
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:]
+# accessing from the end to the position
+
+thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+  print("Yes, 'apple' is in the fruits list")
+# is the string in the list?
+
+numbers = [1, 2, 3, 4, 5]
+if 3 in numbers:
+    print("Yes, 3 is in the numbers list")
+# is the integer in the list
+
+## prac 6
