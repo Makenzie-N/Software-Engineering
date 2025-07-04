9369c3223001d4fed140ca424c768c767905ddbb
Makenzie-N <kenzie.nightingale@education.nsw.gov.au>
2025-07-04 22:37:00 +1000

    Update challenge_D.py

--- challenge_D.py
+++ challenge_D.py
@@ -1,7 +1,7 @@
 weapons = ["Sword", "Axe", "Gun"]
 special_attacks = ["Talon Torrent", "Berserker Blast"]
 
-print("Your weapons are: Sword, Axe, Gun. Your special attacks are: Talon Torrent, Berserker Blast."
+print("Your weapons are: Sword, Axe, Gun. Your special attacks are: Talon Torrent, Berserker Blast.")
 
 wanted_weapon = input("Choose yur weapon: ")
 wanted_attack = input("Choose your special attack: ")
@@ -13,9 +13,26 @@ weapons.append(userWeapon)
 userAttack = input("What special attack would you like to add to your inventory? ")
 special_attacks.append(userAttack)
 
-remove = input("Would you like to remove a weapon from your inventory?")
-if remove == 
 removeWeapon = input("What weapon would you like to remove from your inventory?")
+weapons.remove(removeWeapon)
+removeAttack = input("What special attack would you like to remove from your inventory?")
+special_attacks.remove(removeAttack)
 
 print(len(weapons))
 print(len(special_attacks))
+
+enemies = ["Maniacal Maya", "Frenzied Felix", "Amok Aria", "Barmy Benjamin"]
+
+print(enemies)
+
+wanted_turns = int(input("How many round would you like to play? "))
+current_turn = 1
+
+while current_turn < wanted_turns:
+  wanted_enemy = input("Choose your enemy: ")
+  wanted_weapon = input("Choose your weapon: ")
+  wanted_attack = input("Choose your special attack: ")
+  print(f"You point your {wanted_weapon} at {wanted_enemy}'s head and unleash a {wanted_attack}.")
+  current_turn += 1
+  if current_turn >= wanted turns:
+    break
