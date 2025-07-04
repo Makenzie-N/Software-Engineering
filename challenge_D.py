f69083b5ad7de347ddccbdb99dd65d3578428004
Makenzie-N <kenzie.nightingale@education.nsw.gov.au>
2025-07-04 20:32:30 +1000

    Create challenge_D.py

---
+++ challenge_D.py
@@ -0,0 +1,11 @@
+weapons = ["Sword", "Axe", "Gun"]
+special attacks = ["Talon Torrent", "Berserker Blast"]
+
+print("Your weapons are: Sword, Axe, Gun. Your special attacks are: Talon Torrent, Berserker Blast."
+
+wanted_weapon = input("What weapon would you like to use?")
+wanted_attack = input("What special attack would you like to use?")
+
+print(f"You grip your {wanted_weapon} ready to carve through chaos and trigger a {wanted_attack}.")
+
+weapons.append
