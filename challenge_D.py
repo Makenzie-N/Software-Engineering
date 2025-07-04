75c3115e278d211ed6aa5048c6bd7d932f0b38c1
Makenzie-N <kenzie.nightingale@education.nsw.gov.au>
2025-07-04 21:25:51 +1000

    Update challenge_D.py

--- challenge_D.py
+++ challenge_D.py
@@ -1,11 +1,21 @@
 weapons = ["Sword", "Axe", "Gun"]
-special attacks = ["Talon Torrent", "Berserker Blast"]
+special_attacks = ["Talon Torrent", "Berserker Blast"]
 
 print("Your weapons are: Sword, Axe, Gun. Your special attacks are: Talon Torrent, Berserker Blast."
 
-wanted_weapon = input("What weapon would you like to use?")
-wanted_attack = input("What special attack would you like to use?")
+wanted_weapon = input("Choose yur weapon: ")
+wanted_attack = input("Choose your special attack: ")
 
 print(f"You grip your {wanted_weapon} ready to carve through chaos and trigger a {wanted_attack}.")
 
-weapons.append
+userWeapon = input("What weapon would you like to add to your inventory? ")
+weapons.append(userWeapon)
+userAttack = input("What special attack would you like to add to your inventory? ")
+special_attacks.append(userAttack)
+
+remove = input("Would you like to remove a weapon from your inventory?")
+if remove == 
+removeWeapon = input("What weapon would you like to remove from your inventory?")
+
+print(len(weapons))
+print(len(special_attacks))
