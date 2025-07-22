46c47482de1106cdcd8a90bb7c180262564ed586
Makenzie-N <kenzie.nightingale@education.nsw.gov.au>
2025-07-22 15:05:46 +1000

    parameters vs arguments

---
+++ functions.py
@@ -0,0 +1,24 @@
+def hello_world(): #defines the function hello_world
+    print("Hello world!")
+
+hello_world() #calls function
+    
+
+#num1 and num2 are just place holders/parameters
+def sum(num1=0, num2=0): #both numbers are automatically 0 if only one (or no) argument is entered below
+    if (type(num1) is not int or type(num2) is not int):
+        return 0
+    return num1 + num2 #returns whichever arguments are inputed e.g if we inoput 2, 3 it should return as 5
+
+total = sum() #if eg "a" is entered python will return "none", if nothing is entered error will appear (would only occur if numbers weren't defined as automatically 0 above)
+print(total)
+
+
+def multiple_items(*args): #args = arguments
+    print(args)
+    print(type(args))
+
+multiple_items("Dave", "John", "Sarah")
+
+def mult_named_items(**kwargs): #kwargs = keyword arguments
+    print(kwargsporint(type(kwargs)))
--- pracRocket.py
+++ pracRocket.py
@@ -4,11 +4,11 @@ rocketLocation = 0
 while rocketLocation <= maxHeight:
     print(rocketLocation)
     rocketLocation = rocketLocation + 1
-if rocketHeight >= maxHeight:
+if rocketLocation >= maxHeight:
     print("Max height reached.")
 while rocketLocation > 0:
     print("Rocket falling...")
     rocketLocation = rocketLocation - 1
     print(f"New Location is {rocketLocation}")
-if rocketLocation = 0
+if rocketLocation <= 0:
     print("Rocket has landed")
