94790cf04a0b311af2a1d1e939f9aefa3f0e6917
Makenzie-N <kenzie.nightingale@education.nsw.gov.au>
2025-08-13 13:54:36 +1000

    13/8/25

--- assesment_task.py
+++ assesment_task.py
@@ -1,3 +1,12 @@
+# author - your name
+# title - your game title
+# description - one sentence to explain your game to the player
+
+import os
+import colorama
+import time
+import sys
+
 rooms = ["Abstract Art", "Stunning Sculptures", "Interactive Illusions"]
 paintings = {"Abstract art": ["The Playground", "Kookakoala", "Graffiti"],
              "Stunning Sculptures": ["Isaac Newton", "Taylor Swift", "Ablbert Einstein", "Mr Smith"],
@@ -5,11 +14,43 @@ paintings = {"Abstract art": ["The Playground", "Kookakoala", "Graffiti"],
 }
 tour_guides = ["Tommy", "Reece", "Ben", "Holly"]
 
+
+def clearScreen():
+    os.system('cls')
+
+def colourText(text, colour):
+    print(colour + text)
+
+
 print("Welcome to the 'Museum of Mastery'!")
-tour_guide = str(input(f"We have 4 very lovely and keen tour guides available at the moment {tour_guides}, who would you like to show you around today? "))
+input("Press enter to continue... ")
+
+clearScreen()
+
+tour_guide = str(input("We have 4 very lovely and keen tour guides available at the moment (Tommy, Reece, Ben and Holly), who would you like to show you around today? "))
 if tour_guide in tour_guides:
-    print(f"Sounds great! {tour_guide} will be showing you around today.")
+    print(f"Sounds great! {tour_guide} will be showing you around today. ")
 else:
-    print(f"Tour guide not an options, please select one of the following 4: {tour_guides}")
-    
-user_art = input(f"Before we get started we would like to offer you a position in our amazing art compition 'Time for Tales', this competition is in the last room of our museum so if you accept {tour_guide} will take you there at the end of your session.")
+    while tour_guide not in tour_guides:
+        str(input(f"Tour guide not an options, please select one from the following 4: Tommy, Reece, Ben, Holly. "))
+        for tour_guide in tour_guides:
+            print(f"Sounds great! {tour_guide} will be showing you around today. ")
+            break
+input()
+clearScreen()
+
+user_art = input(f"Before we get started we would like to offer you a position in our amazing art compition 'Time for Tales'. If you accept, {tour_guide} will take you there at the end of your session to create and submit your artwork.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+sys.exit()
